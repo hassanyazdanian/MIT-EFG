9028eb33574a59ffb871b3f7910f78845aaed736
hassanyazdanian <53097233+hassanyazdanian@users.noreply.github.com>
2024-05-12 12:46:01 +0330

    Delete Numerical_solution/FE_EFG directory

--- Numerical_solution/FE_EFG/domain.m
+++
@@ -1,8 +0,0 @@
-%-------------------------------------------------------------------------
-function v = domain(gpos,x,dm,nnodes)
-%DETERMINES NODES IN DOMAIN OF INFLUENCE OF POINT GPOS
-dif = gpos*ones(1,nnodes)-x;
-a = dm-abs(dif);
-b = [a;zeros(1,nnodes)];
-c = all(b>=-100*eps);
-v = find(c);
